e2c9649d2d8e22299164f01e6d962fa4068d9565
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-06-09 12:36:39 +0300

    Add project files.

---
+++ 09.06.2024.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Mutex mutex = new Mutex(false, "ThreeInstancesMutex");
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        bool isNewInstance;
+        mutex = new Mutex(true, "ThreeInstancesMutex", out isNewInstance);
+
+        if (!isNewInstance)
+        {
+            Console.WriteLine("Додаток може бути запущений тільки в трьох копіях. Третя копія закривається.");
+            return;
+        }
+
+        Console.WriteLine("Додаток запущено.");
+        Console.WriteLine("Натисніть будь-яку клавішу для завершення програми...");
+        Console.ReadKey();
+
+        mutex.ReleaseMutex();
+    }
+}
