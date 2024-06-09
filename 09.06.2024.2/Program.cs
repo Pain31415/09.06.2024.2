ed594d7e9326a3878a1da7cb70ff11738e063285
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-06-09 12:36:39 +0300

    Add project files.

---
+++ 09.06.2024.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static SemaphoreSlim semaphore = new SemaphoreSlim(3);
+    static Random random = new Random();
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Task[] tasks = new Task[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            int threadIndex = i;
+            tasks[threadIndex] = Task.Run(() => ThreadWork(threadIndex));
+        }
+
+        Task.WaitAll(tasks);
+
+        Console.WriteLine("Усі потоки завершили роботу.");
+        Console.WriteLine("Натисніть будь-яку клавішу для завершення програми...");
+        Console.ReadKey();
+    }
+
+    static void ThreadWork(int threadIndex)
+    {
+        semaphore.Wait();
+
+        try
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Потік {threadIndex} починає роботу");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Потік {threadIndex}: {random.Next(1, 100)}");
+                Thread.Sleep(100);
+            }
+        }
+        finally
+        {
+            Console.ResetColor();
+            Console.WriteLine($"Потік {threadIndex} завершує роботу");
+            semaphore.Release();
+        }
+    }
+}
